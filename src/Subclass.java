9f9e6a9fc8536ee3090252b6b3a506849479fa82
Leting Cai <letingc@nycstudents.net>
2023-03-21 12:09:25 -0400

    Second Commit

--- src/Subclass.java
+++ src/Subclass.java
@@ -1,2 +1,10 @@
-public class Subclass {
+public class Subclass extends Superclass{
+    private String SubclassString;
+
+    public Subclass(String string, boolean Boolean, int integer, double Double, String SubclassString){
+        super(string, Boolean, integer, Double);
+        this.SubclassString = SubclassString;
+    }
+
+    public void subclassMethod(){}
 }
---
+++ src/Subclass2.java
@@ -0,0 +1,12 @@
+public class Subclass2 extends Superclass{
+    private int subclass2int;
+
+    public Subclass2(String string, boolean Boolean, int integer, double Double, int subclass2int){
+        super(string, Boolean, integer, Double);
+        this.subclass2int = subclass2int;
+    }
+
+    public void subClass2Method(){
+
+    }
+}
